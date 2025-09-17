ca62e0816ae2a86b820cbf77c7696b3b3fafc008
Jorge <jorge.attie@meta.com.br>
2025-09-17 14:23:24 -0300

    first commit

---
+++ products/webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugins = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 8081,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "products",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./ProductsIndex": "./src/index",
+      },
+    }),
+    new HtmlWebpackPlugins({
+      template: "./public/index.html",
+    }),
+  ],
+};
